39aea7c7d2bc7847e4db3e8ab8868752088cee84
Keyur1291 <pkeyur2138@gmail.com>
2022-01-29 03:04:21 +0530

    Improvements in App bar

--- app/src/main/java/com/android/internshipportal/student_home.java
+++ app/src/main/java/com/android/internshipportal/student_home.java
@@ -1,74 +1,93 @@
 package com.android.internshipportal;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class student_home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class navigation_drawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, profile.onFragmentSelected {
 
     MaterialButton logout;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     FirebaseAuth mAuth;
-    FirebaseFirestore firebaseFirestore;
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
-    MaterialToolbar toolbar;
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-        setContentView(R.layout.activity_student_home);
+        setContentView(R.layout.activity_navigation_drawer);
 
         mAuth = FirebaseAuth.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
 
         logout = findViewById(R.id.logOut);
         logout.setOnClickListener(View -> {
-            MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(student_home.this, R.style.ThemeOverlay_App_MaterialAlertDialog);
-            dialogBuilder.setTitle("Logout");
-            dialogBuilder.setMessage("Are you sure want to Logout");
-            dialogBuilder.setPositiveButton("Yes", (dialog, which) -> {
-                mAuth.signOut();
-                Toast.makeText(student_home.this, "Logged Out Successfully", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(student_home.this, login.class));
-            }).setNegativeButton("No", (dialog, which) -> dialog.dismiss());
-            dialogBuilder.show();
+            mAuth.signOut();
+            Toast.makeText(navigation_drawer.this, "Logged Out Successfully", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(navigation_drawer.this, login.class));
         });
 
-        toolbar = findViewById(R.id.appbar);
+        Toolbar toolbar = findViewById(R.id.appbar);
         setSupportActionBar(toolbar);
-        toolbar.setNavigationOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
 
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
-        navigationView.setCheckedItem(R.id.home);
 
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openNavDrawer,
+                R.string.closeNavDrawer
+        );
 
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment_container, new main_student());
+        fragmentTransaction.add(R.id.fragment_container, new home());
         fragmentTransaction.commit();
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user == null) {
+            startActivity(new Intent(navigation_drawer.this, login.class));
+        }
     }
 
     @Override
@@ -83,35 +102,49 @@ public class student_home extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         drawerLayout.closeDrawer(GravityCompat.START);
-        if (item.getItemId() == R.id.home) {
+        if(item.getItemId() == R.id.home) {
             fragmentManager = getSupportFragmentManager();
             fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new main_student());
+            fragmentTransaction.replace(R.id.fragment_container, new home());
             fragmentTransaction.commit();
         }
 
-        if (item.getItemId() == R.id.about) {
+        if(item.getItemId() == R.id.profile) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new profile());
+            fragmentTransaction.commit();
+        }
+
+        if(item.getItemId() == R.id.cdetails) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new company_details());
+            fragmentTransaction.commit();
+        }
+
+        if(item.getItemId() == R.id.about) {
             fragmentManager = getSupportFragmentManager();
             fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.fragment_container, new about());
             fragmentTransaction.commit();
         }
 
-        if (item.getItemId() == R.id.NOC) {
+        if(item.getItemId() == R.id.NOC) {
             fragmentManager = getSupportFragmentManager();
             fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.fragment_container, new noc_letter());
             fragmentTransaction.commit();
         }
 
-        if (item.getItemId() == R.id.certi) {
-            fragmentManager = getSupportFragmentManager();
-            fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new completion_certi());
-            fragmentTransaction.commit();
-        }
-
         return true;
     }
 
+    @Override
+    public void onButtonSelected() {
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new edit_profile());
+        fragmentTransaction.commit();
+    }
 }
