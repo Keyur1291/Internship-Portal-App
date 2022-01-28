027018a42169b41b99a8d3328b4a44588948f4ef
Keyur1291 <pkeyur2138@gmail.com>
2022-01-29 03:04:21 +0530

    Improvements in App bar

--- app/src/main/java/com/android/internshipportal/navigation_drawer.java
+++ app/src/main/java/com/android/internshipportal/navigation_drawer.java
@@ -1,17 +1,26 @@
 package com.android.internshipportal;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -24,7 +33,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class navigation_drawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, profile.onFragmentSelected {
 
-    private Toolbar toolbar;
     MaterialButton logout;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -32,9 +40,11 @@ public class navigation_drawer extends AppCompatActivity implements NavigationVi
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(R.layout.activity_navigation_drawer);
 
         mAuth = FirebaseAuth.getInstance();
@@ -46,7 +56,7 @@ public class navigation_drawer extends AppCompatActivity implements NavigationVi
             startActivity(new Intent(navigation_drawer.this, login.class));
         });
 
-        toolbar = findViewById(R.id.appbar);
+        Toolbar toolbar = findViewById(R.id.appbar);
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
