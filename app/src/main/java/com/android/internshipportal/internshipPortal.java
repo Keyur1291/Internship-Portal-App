5b1063f9ce2dd593cb9fcf09c4235e24d989bebb
Keyur1291 <pkeyur2138@gmail.com>
2022-01-28 19:19:52 +0530

    Implemented Wallpaper based theming

---
+++ app/src/main/java/com/android/internshipportal/internshipPortal.java
@@ -0,0 +1,12 @@
+package com.android.internshipportal;
+
+import android.app.Application;
+import com.google.android.material.color.DynamicColors;
+
+public class internshipPortal extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
+}
--- app/src/main/java/com/android/internshipportal/login.java
+++ app/src/main/java/com/android/internshipportal/login.java
@@ -1,9 +1,5 @@
 package com.android.internshipportal;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.Navigation;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,21 +8,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class login extends AppCompatActivity {
