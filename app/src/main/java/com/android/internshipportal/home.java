56edadadbffab9978fa9b181259272aeb90b3767
Jainam Shah <jainamtpnpt304@gmail.com>
2022-01-29 11:26:49 +0530

    Added view binding adn now can click profile

--- app/src/main/java/com/android/internshipportal/home.java
+++ app/src/main/java/com/android/internshipportal/home.java
@@ -1,21 +1,44 @@
 package com.android.internshipportal;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class home extends Fragment {
+import com.android.internshipportal.databinding.FragmentHomeBinding;
 
+
+public class home extends Fragment {
+    FragmentHomeBinding binding;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        return view;
+        binding=FragmentHomeBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.profileLayout.setOnClickListener(v->{
+            Fragment profileFragment=new profile();
+            FragmentManager fragmentManager=getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container,profileFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding=null;
+    }
 }
