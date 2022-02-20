ee76010220a2867b33464939617240d61b9bfa9a
Keyur1291 <pkeyur2138@gmail.com>
2022-02-20 14:34:45 +0530

    Replaced buttons on home screen with bottom navigation

--- app/src/main/java/com/android/internshipportal/home.java
+++ app/src/main/java/com/android/internshipportal/home.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,29 +18,11 @@ import com.android.internshipportal.databinding.FragmentHomeBinding;
 
 public class home extends Fragment {
     FragmentHomeBinding binding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding=FragmentHomeBinding.inflate(getLayoutInflater());
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
         return binding.getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        binding.profileLayout.setOnClickListener(v->{
-            Fragment profileFragment=new profile();
-            FragmentManager fragmentManager=getActivity().getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container,profileFragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
-        });
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        binding=null;
     }
 }
