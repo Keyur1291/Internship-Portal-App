b7f6cf5a471eb14e67fea85f29d9cb1ee770d205
Keyur1291 <pkeyur2138@gmail.com>
2022-02-20 17:38:34 +0530

    Added internship form layout

--- app/src/main/java/com/android/internshipportal/internshipform.java
+++ app/src/main/java/com/android/internshipportal/internshipform.java
@@ -1,10 +1,6 @@
 package com.android.internshipportal;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.android.internshipportal.databinding.FragmentHomeBinding;
-
-
-public class home extends Fragment {
-    FragmentHomeBinding binding;
-
+public class internshipform extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentHomeBinding.inflate(getLayoutInflater());
-        return binding.getRoot();
+        View view = inflater.inflate(R.layout.fragment_internshipform, container, false);
+        return view;
     }
 }
