73147ec3aafa392004d8570e5caae3dc36ed4efb
Keyur1291 <pkeyur2138@gmail.com>
2022-03-27 17:46:57 +0530

    Designed Admin panel. Admin can add Users. Admin can check Users list. Admin can add Company details. Admin can check company ist. Admin can add faculty. Improved layouts in all activities. Fixed an  issue where faculty user was not able to login.

--- app/src/main/java/com/android/internshipportal/admin_home.java
+++ app/src/main/java/com/android/internshipportal/admin_home.java
@@ -1,28 +1,34 @@
 package com.android.internshipportal;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.WindowCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.textview.MaterialTextView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class admin_home extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
     MaterialToolbar toolbar;
     MaterialButton logout;
     FirebaseAuth mAuth;
+    FirebaseFirestore fstore;
+    int ucounter;
+    MaterialCardView users, company, addStudent, addFaculty, addcompany;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +37,43 @@ public class admin_home extends AppCompatActivity {
         setContentView(R.layout.activity_admin_home);
 
         mAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
         logout = findViewById(R.id.logOut);
         logout.setOnClickListener(View -> {
             mAuth.signOut();
             Toast.makeText(admin_home.this, "Logged Out Successfully", Toast.LENGTH_SHORT).show();
             startActivity(new Intent(admin_home.this, login.class));
+            finish();
         });
 
         toolbar = findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
 
-        bottomNavigationView = findViewById(R.id.bnav);
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            if(item.getItemId() == R.id.nav_profile) {
-                startActivity(new Intent(admin_home.this, students_list.class));
-            }
-            return true;
+        users = findViewById(R.id.usersbtn);
+        users.setOnClickListener(View -> {
+            startActivity(new Intent(admin_home.this, students_list.class));
         });
+
+        company = findViewById(R.id.companybtn);
+        company.setOnClickListener(View -> {
+            startActivity(new Intent(admin_home.this, company_list.class));
+        });
+
+        addStudent = findViewById(R.id.addstudent);
+        addStudent.setOnClickListener(View -> {
+            startActivity(new Intent(admin_home.this, add_student.class));
+        });
+
+        addFaculty = findViewById(R.id.addfaculty);
+        addFaculty.setOnClickListener(View -> {
+            startActivity(new Intent(admin_home.this, add_faculty.class));
+        });
+
+        addcompany = findViewById(R.id.addcompany);
+        addcompany.setOnClickListener(View -> {
+            startActivity(new Intent(admin_home.this, add_company.class));
+        });
+
     }
 }
---
+++ app/src/main/java/com/android/internshipportal/recycle_getter_setter.java
@@ -0,0 +1,102 @@
+package com.android.internshipportal;
+
+public class recycle_getter_setter {
+
+    String name, enrollment, department, mobile, email, Cname, subject, Caddress, Cmobile, Cemail;
+
+    public recycle_getter_setter() {
+    }
+
+    public recycle_getter_setter(String name, String enrollment, String department, String mobile, String email, String Cname, String subject, String Caddress, String Cmobile, String Cemail) {
+        this.name = name;
+        this.enrollment = enrollment;
+        this.department = department;
+        this.mobile = mobile;
+        this.email = email;
+        this.Cname = Cname;
+        this.subject = subject;
+        this.Caddress = Caddress;
+        this.Cmobile = Cmobile;
+        this.Cemail = Cemail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(String enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCname() {
+        return Cname;
+    }
+
+    public void setCname(String cname) {
+        Cname = cname;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getCaddress() {
+        return Caddress;
+    }
+
+    public void setCaddress(String caddress) {
+        Caddress = caddress;
+    }
+
+    public String getCmobile() {
+        return Cmobile;
+    }
+
+    public void setCmobile(String cmobile) {
+        Cmobile = cmobile;
+    }
+
+    public String getCemail() {
+        return Cemail;
+    }
+
+    public void setCemail(String cemail) {
+        Cemail = cemail;
+    }
+}
