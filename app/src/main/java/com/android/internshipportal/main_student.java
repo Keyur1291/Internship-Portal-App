8125402b70c297e824d97258c977631330f38042
Keyur1291 <pkeyur2138@gmail.com>
2022-03-28 04:09:51 +0530

    Most of the click event works now. User can edit or delete their profile. Fixed student list layout. Fixed company list layout. Designed activities of faculty user. Designed noc download page. Designed certi upload page. Improvements in overall UI. And much more that i can't remember probably XD.

---
+++ app/src/main/java/com/android/internshipportal/main_student.java
@@ -0,0 +1,46 @@
+package com.android.internshipportal;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.card.MaterialCardView;
+
+public class main_student extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_student, container, false);
+        MaterialCardView profile, company, prevcompany, internship;
+
+        profile = view.findViewById(R.id.profilebtn);
+        profile.setOnClickListener(View -> {
+            startActivity(new Intent(getActivity().getApplicationContext(), com.android.internshipportal.profile.class));
+        });
+
+        company = view.findViewById(R.id.companydtls);
+        company.setOnClickListener(View -> {
+            startActivity(new Intent(getActivity().getApplicationContext(), com.android.internshipportal.company_details.class));
+        });
+
+        prevcompany = view.findViewById(R.id.prevcompanies);
+        prevcompany.setOnClickListener(View -> {
+            startActivity(new Intent(getActivity().getApplicationContext(), com.android.internshipportal.company_list.class));
+        });
+
+        internship = view.findViewById(R.id.applyinternship);
+        internship.setOnClickListener(View -> {
+            startActivity(new Intent(getActivity().getApplicationContext(), com.android.internshipportal.internship_form.class));
+        });
+
+        return view;
+    }
+}
